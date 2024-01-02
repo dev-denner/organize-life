741d5dba1d55ab6c9ad4c6f9292a738a71b2743b
Denner Fernandes <dev.denner@outlook.com>
2024-01-02 19:37:29 -0300

    feat: ✨ add repository

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,72 +1,27 @@
-import prisma from '@/libs/prisma';
+import { create, del, listAll, listOne, update } from '../repository';
+
+const entity = 'user';
 
 export const GET = async (request: Request) => {
-  try {
-    const { searchParams } = new URL(request.url);
-    const external_code = searchParams.get('external_code');
-    if (external_code) {
-      const user = await prisma.users.findFirst({
-        where: { external_code },
-      });
-      if (!user) {
-        throw { status: 404, message: 'User not found' }
-      }
-      return new Response(JSON.stringify({ data: user, success: true }), { status: 200 });
-    }
-    const users = await prisma.users.findMany();
-    return new Response(JSON.stringify({ data: users, success: true }), { status: 200 });
-  } catch (error: any) {
-    return handleError(error);
-  } finally {
-    await prisma.$disconnect();
+  const { searchParams } = new URL(request.url);
+  const external_code = searchParams.get('external_code');
+  if (external_code) {
+    return listOne(entity, { external_code })
   }
+  return listAll(entity);
 };
 
 export const POST = async (request: Request) => {
   const body = await request.json();
-  try {
-    const user = await prisma.users.create({
-      data: body
-    });
-    return new Response(JSON.stringify({ data: user, success: true }), { status: 200 });
-  } catch (error: any) {
-    return handleError(error);
-  } finally {
-    await prisma.$disconnect();
-  }
+  return create(entity, body);
 };
 
 export const PUT = async (request: Request) => {
   const body = await request.json();
-  try {
-    const user = await prisma.users.update({
-      where: { id: body.id },
-      data: body
-    });
-    return new Response(JSON.stringify({ data: user, success: true }), { status: 200 });
-  } catch (error: any) {
-    return handleError(error);
-  } finally {
-    await prisma.$disconnect();
-  }
+  return update(entity, body);
 };
 
 export const DELETE = async (request: Request) => {
   const body = await request.json();
-  try {
-    const user = await prisma.users.delete({
-      where: { id: body.id },
-    });
-    return new Response(JSON.stringify({ data: user, success: true }), { status: 200 });
-  } catch (error: any) {
-    return handleError(error);
-  } finally {
-    await prisma.$disconnect();
-  }
-};
-
-const handleError = (error: any) => {
-  const status = error?.status ?? 500;
-  const message = error?.message ?? 'error in user api';
-  return new Response(JSON.stringify({ message, error, success: false }), { status });
+  return del(entity, body);
 };
