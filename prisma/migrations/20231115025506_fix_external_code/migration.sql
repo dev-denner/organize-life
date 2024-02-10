00d3580f15bd02864556c243fba9691c4f410895
Denner Fernandes <dev.denner@outlook.com>
2024-02-10 16:53:50 -0300

    chore: 🚧 undo project in next

--- prisma/migrations/20231115025506_fix_external_code/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "movements" ALTER COLUMN "external_code" SET DATA TYPE TEXT;
-
--- AlterTable
-ALTER TABLE "purchase_items" ALTER COLUMN "external_code" SET DATA TYPE TEXT;
-
--- AlterTable
-ALTER TABLE "purchases" ALTER COLUMN "external_code" SET DATA TYPE TEXT;
